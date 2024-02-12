a4e2897441f34974629c61094b553bcee6fc1e7d
Filipe Pina <636320+fopina@users.noreply.github.com>
2024-02-12 21:16:50 +0000

    Update grafana-print-friendly.js

--- grafana-print-friendly.js
+++ grafana-print-friendly.js
@@ -22,9 +22,9 @@ function waitForElement(selector, callback, maxtries = false, interval = 100) {
   
   function doIt() {
     var shareNode = document.querySelectorAll('button[aria-label="Share dashboard"]')[0];
-    // FIXME: remove deep clone and add printer svg path
     var newNode = shareNode.cloneNode(true);
     newNode.setAttribute("aria-label", "Load for printing");
+    newNode.querySelector('svg').innerHTML = '<path fill="none" d="M0 0h24v24H0z"></path> <path d="M6 19H3a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h3V3a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v4h3a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-3v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-2zm0-2v-1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v1h2V9H4v8h2zM8 4v3h8V4H8zm0 13v3h8v-3H8zm-3-7h3v2H5v-2z"></path>';
     shareNode.insertAdjacentElement("afterend", newNode);
     newNode.onclick = () => {
       // credits to https://github.com/grafana/grafana/issues/13437#issuecomment-1375790162
